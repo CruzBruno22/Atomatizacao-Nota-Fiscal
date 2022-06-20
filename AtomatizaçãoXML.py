f376957504fe3f42a4fb8cf2e7ca8328124c4829
CruzBruno22 <107863024+CruzBruno22@users.noreply.github.com>
2022-06-20 10:55:23 -0300

    Update AtomatizaçãoXML.py

--- AtomatizaçãoXML.py
+++ AtomatizaçãoXML.py
@@ -1,1 +1,111 @@
-#TEste
+# Importações Bibliotecas
+
+import pyautogui
+import time
+
+# Perguntar quantos XML no dia
+
+Nxml = 0
+TotalXML = int (input ("Quantos XML temos hoje?"))
+pyautogui.PAUSE = 1 # Tempo de delay Global(pode ser alterado)
+
+# Entrar no site da Fazenda
+
+pyautogui.hotkey ("ctrl","t") # Abrir uma nova aba 
+pyautogui.click (x=638, y=85)
+pyautogui.write ("https://www.nfe.fazenda.gov.br")
+pyautogui.press ("enter")
+pyautogui.moveTo (x=387, y=396)
+pyautogui.click (x=336, y=435)
+
+#Entrar No Word Lista de xml
+
+pyautogui.hotkey ("win", "s")
+pyautogui.write ("XML")
+pyautogui.press ("enter")
+time.sleep (6)
+
+# Download Primeiro XML ( Certificado )
+
+pyautogui.click (x=430, y=424) 
+pyautogui.hotkey ("ctrl", "x") # Recortar o Primeiro XML do Word
+pyautogui.keyDown ("alt") # Presionar tecla Alt
+pyautogui.press ("tab") # Clicar TAB
+pyautogui.keyUp ("alt") # Soltar tecla Alt
+pyautogui.click (x=569, y=657)
+pyautogui.hotkey ("ctrl", "v") # Colar XML
+pyautogui.click (x=585, y=741) # Clicar em Captcha
+pyautogui.click (x=738, y=838) # Clicar em Continuar
+pyautogui.click (x=753, y=605) # Clicar em Download 1
+pyautogui.press ("enter") # Confirmar Download
+pyautogui.click (x=498, y=606) # Clicar em Download 2
+pyautogui.press ("enter") # Confirmar Download
+pyautogui.click (x=611, y=1043) # Clicar no PopUp
+pyautogui.press ("tab")
+pyautogui.press ("enter")
+pyautogui.moveTo (x=387, y=396)
+pyautogui.click (x=336, y=435)
+
+Nxml = Nxml + 1
+
+# Loop para demais Downloads
+
+while Nxml < TotalXML: 
+    pyautogui.keyDown ("alt") # Presionar tecla Alt
+    pyautogui.press ("tab") # Clicar TAB
+    pyautogui.keyUp ("alt") # Soltar tecla Alt
+    pyautogui.click (x=430, y=424) 
+    pyautogui.hotkey ("ctrl", "x") # Recortar o Primeiro XML do Word
+    pyautogui.keyDown ("alt") # Presionar tecla Alt
+    pyautogui.press ("tab") # Clicar TAB
+    pyautogui.keyUp ("alt") # Soltar tecla Alt
+    pyautogui.click (x=569, y=657)
+    pyautogui.hotkey ("ctrl", "v") # Colar XML
+    pyautogui.click (x=585, y=741) # Clicar em Captcha
+    pyautogui.click (x=738, y=838) # Clicar em Continuar
+    pyautogui.click (x=893, y=606) # Clicar em Download 1
+    pyautogui.press ("enter") # Confirmar Download
+    pyautogui.click (x=677, y=608) # Clicar em Download 2
+    pyautogui.press ("enter") # Confirmar Download
+    pyautogui.moveTo (x=387, y=396)#prepara o proximo
+    pyautogui.click (x=336, y=435)
+    Nxml = Nxml + 1
+    
+# Automatizar Email
+
+pyautogui.PAUSE = 1
+pyautogui.hotkey ("win", "e") # Explorar Pastas
+pyautogui.press ("tab")
+pyautogui.press ("tab")
+pyautogui.press ("tab")
+pyautogui.press ("tab")
+pyautogui.press ("tab")
+pyautogui.press ("down", presses = 2) # Ir para pasta Downloads
+pyautogui.press ("enter")
+pyautogui.hotkey ("Ctrl", "a") # Selecionar Tudo
+pyautogui.hotkey ("Ctrl", "x") # Recortar Tudo
+pyautogui.press ("win") 
+pyautogui.write ("outlook")
+pyautogui.press ("enter")
+time.sleep (11)
+pyautogui.hotkey ("Ctrl", "Shift" , "m") # Abrir um novo Email
+time.sleep (2)
+pyautogui.write ("Trevo Recebimentofiscal (GS/OSP22-LA) <Recebimentofiscal.Trevo@br.bosch.com>")
+pyautogui.press ("tab")
+pyautogui.press ("tab")
+pyautogui.press ("tab")
+pyautogui.press ("tab")
+pyautogui.write ("xml") #Assunto
+pyautogui.press ("tab")
+pyautogui.hotkey ("Ctrl", "v") # Colar Tudo (XML)
+time.sleep (15)
+pyautogui.hotkey ('ctrl', 'enter') # Enviar o Email
+time.sleep (15)  
+pyautogui.click (x=1083, y=738) # Clicar em NO para enviar
+time.sleep (15)
+
+# Alerta sobre finalização
+
+pyautogui.alert ("FINALIZADO", "Automatização XML", "Confirmar")
+
+
